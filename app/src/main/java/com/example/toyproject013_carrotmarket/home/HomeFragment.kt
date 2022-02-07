6a634ce418645ba2841377b6a5fb11013d5a7b18
kimjinsub <hikm9985@naver.com>
2022-02-07 21:31:36 +0900

    상품 모록 페이지 UI 구성

--- app/src/main/java/com/example/toyproject013_carrotmarket/home/HomeFragment.kt
+++ app/src/main/java/com/example/toyproject013_carrotmarket/home/HomeFragment.kt
@@ -1,7 +1,30 @@
 package com.example.toyproject013_carrotmarket.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.toyproject013_carrotmarket.R
+import com.example.toyproject013_carrotmarket.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding =fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply{
+            add(ArticleModel("0","자두",100000,"3000원",""))
+            add(ArticleModel("0","아이스크림",200000,"7000원",""))
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+    }
 }
